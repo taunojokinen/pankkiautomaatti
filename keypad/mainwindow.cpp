a7d9061ff4146dca473ba7b8f461a1ab9297ce09
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2023-10-24 17:21:54 +0300

    keypad valmis

--- keypad/mainwindow.cpp
+++ keypad/mainwindow.cpp
@@ -6,8 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-  //  ui->pushButton->setStyleSheet("QPushButton{background-color :rgb(211,211,111); }");
- //   ui->pushButton->setStyleSheet("QPushButton{border-radius: 5px;}");
+
 
 
 }
@@ -15,5 +14,18 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    QLinearGradient linearGradient(QPointF(200,100), QPointF(50,300));
+    linearGradient.setColorAt(0, QColor(236,236,236,255));
+    linearGradient.setColorAt(0.5, QColor(224,224,224,255));
+    linearGradient.setColorAt(1, QColor(215,215,215,255));
+    QRect rectangle (50,50,430,330);
+    painter.fillRect(rectangle,linearGradient);
+    painter.drawRoundedRect(rectangle, 20.0, 15.0);
 }
 
--- keypad/mainwindow.h
+++ keypad/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    virtual void paintEvent(QPaintEvent * event);
 
 private:
     Ui::MainWindow *ui;
